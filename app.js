186ab49e15220a6c4be88118d92b711cd396f96b
Brenda Pereira <brendacamara@outlook.com>
2020-05-30 04:05:37 -0300

    configurando o passport

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const Postagem = mongoose.model('postagens')
 require('./models/Categoria')
 const Categoria = mongoose.model('categorias')
 const usuarios = require('./routes/usuario')
+const passport = require('passport')
+require('./config/auth')(passport)
 
 //config
     //session
